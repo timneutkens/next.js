05cabc245afd44e538eac39a32e3978c31e5c143
Tim Neutkens <timneutkens@me.com>
2021-06-30 16:03:19 +0200

    Remove chokidar dep

--- packages/next/build/webpack-config.ts
+++ packages/next/build/webpack-config.ts
@@ -843,7 +843,6 @@ export default async function getBaseWebpackConfig(
         '**/.git/**',
         '**/node_modules/**',
         '**/.next/**',
-        // can be removed after https://github.com/paulmillr/chokidar/issues/955 is released
         emacsLockfilePattern,
       ],
     },
